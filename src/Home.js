5fa810228397be054c276a8814c6e259ff95a5c0
miltonr-s <miltinjs.dev@gmail.com>
2022-10-21 23:02:44 -0300

    updates and design fixes

--- src/Home.js
+++ src/Home.js
@@ -1,163 +1,82 @@
-import React, { Components } from 'react'
-import { Menu, Button, HeaderImage, ContainerShirt, ButtonShirt, Shirt, MenuNav, Modal, LoginModal } from './styles'
-import GlobalStyle from './styles/global'
+import React, { useState } from 'react'
+import { Modal, CloseButton } from 'react-bootstrap';
+import { Menu, LoginModal, ContainerShirt, ButtonShirt, Shirt } from './styles'
 import { FaSyncAlt } from 'react-icons/fa'
+import GlobalStyle from './styles/global'
 
-
+import ShirtFront from './imagem/texturaFrente.png';
 
 function Home() {
+  const [resetModalShow, setResetModalShow] = useState(false);
+  
+  
   return (
-    <>
-      <Menu>
-        <Button>
-          <strong></strong>
-        </Button>
-        <HeaderImage>
-          <strong>logo</strong>
-        </HeaderImage>
-        <ContainerShirt>
-          <Shirt>
-            <img src="https://o.remove.bg/downloads/3b2e793f-e594-4b34-8a2b-c101428304ac/image-removebg-preview.png" alt="LOGO" />
-          </Shirt>
-          <ButtonShirt>
-            <div style={{ flex: 1, padding: 6, marginLeft: -10, marginTop: -284, }}>
-              <strong style={{ marginLeft: "54px", fontFamily: "Montserrat", padding: "6px" }}>Frente</strong>
-            </div>
-            <div className='square-rotation'>
-              <FaSyncAlt style={{ color: "white", width: "50px", marginTop: "4px", marginLeft: "13px", cursor: "pointer" }} />
-              <strong>
-                <h1 style={{ fontSize: "14px", color: "white", marginLeft: "5", marginTop: "-14px" }}>
-                  girar 360
-                </h1>
-              </strong>
-            </div>
-          </ButtonShirt>
-        </ContainerShirt>
-      </Menu>
-
-
-     
-      <MenuNav>
-        <nav className="sidebar-navigation">
+    <main className="container">
+      <div className="d-flex mx-4 mt-5">
+        <Menu>
           <ul>
             <li className="active">
-
-              <span className="tooltip"></span>
+              <span>Modelos</span>
             </li>
-            <li>
-
-              <span className="tooltip"></span>
+            <li className="active">
+              <span>Cor</span>
             </li>
-            <li>
-
-              <span className="tooltip"></span>
+            <li className="active">
+              <span>Imagem</span>
             </li>
-            <li>
-
-              <span class="tooltip"></span>
-            </li>
-            <li>
-
-              <span class="tooltip"></span>
+            <li className="active">
+              <span>Modelos</span>
             </li>
           </ul>
-        </nav>
-      </MenuNav>
+          <div className="custom">
 
-
-      <Modal>
-
-
-
-
-        <section className="modal-reset">
-          <div style={{ marginLeft: "627px", marginTop: "79px" }} className="button-rotation">
-            <button style={{ borderRadius: "14px", marginTop: "10px", marginLeft: "0", border: "white", width: "173px", height: "36px" }} type="button" class="btn btn-danger btn-lg mt-2 ml-2" data-toggle="modal" data-target="#meuModal">
-              <h1 className='h1-1'>
-                Resetar mudanças
-              </h1>
-            </button>
           </div>
-          <div id="meuModal" className="modal fade" role="dialog">
-            <div className="modal-dialog">
-
-
-              <div className="modal-content">
-
-
-                <div className="modal-header">
-                  <h4 className="modal-title">Camisa modelo bla bla</h4>
-                  <button type="button" className="close" data-dismiss="modal">&times;</button>
-                </div>
-
-
-                <div className="modal-body">
-                  <p>Tem certeza que voce deseja</p>
-                  <p>excluir?</p>
-                </div>
-
-                <div className="modal-footer">
-                  <button type="button" className="btn btn-danger" data-dismiss="modal">Fechar</button>
-                  <button type="button" className="btn btn-warning">Salvar</button>
-                </div>
-
-              </div>
-            </div>
-          </div>
-        </section>
-      </Modal>
-
-      <LoginModal>
-
-        <section className="modal-login">
-          <div className="container">
-            <div style={{ marginTop: "-584px", marginLeft: "1068px" }} className="moldal-pointer">
-              <button style={{ width: "147px" }} type="button" className="btn btn-info btn-warning" data-toggle="modal" data-target="#loginModal">
-                Login
+        </Menu>
+        <section className="d-flex flex-column justify-content-center ms-3">
+          <ContainerShirt>
+            <div className="d-flex justify-content-end">
+              <button
+                className="bg-dark border-0 rounded text-white p-2"
+                style={{
+                  width: "80px",
+                  height: "80px",
+                }}
+              >
+                Girar 180º
               </button>
             </div>
-          </div>
-          <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div className="modal-dialog modal-dialog-centered" role="document">
-              <div className="modal-content">
-                <div className="modal-header border-bottom-0">
-                  <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">×</span>
-                  </button>
-                </div>
-                <div className="modal-body">
-                  <div className="form-title text-center">
-                    <h4>Login</h4>
-                  </div>
-                  <div className="d-flex flex-column text-center">
-                    <form>
-                      <div className="form-group">
-                        <input type="email" className="form-control" id="email1" placeholder="Seu email"></input>
-                      </div>
-                      <div className="form-group">
-                        <input type="password" className="form-control" id="password1" placeholder="Sua senha"></input>
-                      </div>
-                      <button type="button" className="btn btn-info btn-block btn-warning">Login</button>
-                    </form>
-
-                  </div>
-                </div>
-                <div className="modal-footer d-flex justify-content-center">
-                  <div className="signup-section">Crie sua conta <a href="#a" className="text-info">Clique Aqui</a>.</div>
-                </div>
+            <div className="text-center" style={{ flex: 1 }}>
+              <img className="w-75" src={ShirtFront} alt="" />
+            </div>
+            <div className='d-flex justify-content-center'>
+              <div className='bg-primary rounded-pill py-2 px-5'>
+                <strong>Frente</strong>
               </div>
             </div>
-          </div>
+
+          </ContainerShirt>
+          <button 
+            className="btn btn-danger text-white px-4 mt-3 align-self-center"
+            onClick={() => setResetModalShow(true)}
+          >
+            <strong>Resetar mudanças</strong>
+          </button>
         </section>
-
-
-
-      </LoginModal>
-
-
-
+        <Modal show={resetModalShow}>
+          <Modal.Header>
+            <h4 className="modal-title">Camisa modelo</h4>
+            <CloseButton className="shadow-none" onClick={() => setResetModalShow(false)} />
+          </Modal.Header>
+          <Modal.Body>
+            <p>Tem certeza que voce deseja dar o cu?</p>
+          </Modal.Body>
+          <Modal.Footer className="justify-content-stretch">
+            <button type="button" className="btn btn-primary">Salvar</button>
+          </Modal.Footer>
+        </Modal>
+      </div>
       <GlobalStyle />
-    </>
+    </main>
   );
 }
 
